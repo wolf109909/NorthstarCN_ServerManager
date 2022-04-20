68b71db6777f680a1856de5b9a4c3a3426f44446
wolf109909 <1486234278@qq.com>
2022-04-20 16:22:54 +0800

    fuck me

--- NorthstarCN_ServerManager/screencontroller.py
+++ NorthstarCN_ServerManager/screencontroller.py
@@ -2,6 +2,6 @@
 Base library for controlling multiple server instances from "screen"
 Idea: each server is binded to specific screen session with the same random docker name, so we can have multiple instances running at the same time.
 """
-
+import subprocess
 def NewScreenSubProcess(serveruid,startupcommand):
     subprocess.run(["screen", "-S", serveruid , startupcommand])
